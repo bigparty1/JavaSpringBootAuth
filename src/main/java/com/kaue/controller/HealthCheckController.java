c5f847abde4f87cd52f12d9634e991d42500c3f6
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-07-06 20:48:04 -0300

    add operation annotation to health check endpoint

--- src/main/java/com/kaue/controller/HealthCheckController.java
+++ src/main/java/com/kaue/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.kaue.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
     @GetMapping("/check")
+    @Operation(summary = "Verifica se a API está online", description = "Retorna 'OK' se a aplicação estiver rodando corretamente")
     public String healthCheck() {
         return "OK";
     }
