4b9a36a5f7a8e6b1d2b5d819634502ada0549a98
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-07-06 19:11:37 -0300

    commit inicial

---
+++ src/main/java/com/kaue/javaspringbootauth/JavaSpringBootAuthApplication.java
@@ -0,0 +1,13 @@
+package com.kaue.javaspringbootauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaue/javaspringbootauth/JavaSpringBootAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaue.javaspringbootauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
