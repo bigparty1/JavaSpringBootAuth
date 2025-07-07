0523b795fde3e42e0ec175aba2395e53c479a54f
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-07-06 21:35:51 -0300

    add health check controller and security configuration for API access

--- src/main/java/com/kaue/javaspringbootauth/config/SecurityConfig.java
+++ src/main/java/com/kaue/javaspringbootauth/config/SecurityConfig.java
@@ -1,16 +1,21 @@
-package com.kaue.config;
+package com.kaue.javaspringbootauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        System.out.println(">>> SecurityConfig foi carregada!");
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
--- src/main/java/com/kaue/javaspringbootauth/controller/HealthCheckController.java
+++ src/main/java/com/kaue/javaspringbootauth/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.kaue.controller;
+package com.kaue.javaspringbootauth.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
