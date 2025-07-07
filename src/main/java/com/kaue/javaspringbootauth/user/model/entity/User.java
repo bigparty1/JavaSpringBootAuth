1016fd48e6165e069e12d4514eecdf926260a1eb
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-07-07 13:25:08 -0300

    add User entity and SQL script for user table creation

---
+++ src/main/java/com/kaue/javaspringbootauth/user/model/entity/User.java
@@ -0,0 +1,80 @@
+package com.kaue.javaspringbootauth.user.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String hash;
+
+    @Column(nullable = false)
+    private String salt;
+
+    @Column(nullable = false)
+    private String role;
+
+    public User() {}
+
+    public User(String username, String hash, String salt, String role) {
+        this.username = username;
+        this.hash = hash;
+        this.salt = salt;
+        this.role = role;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", hash='" + hash + '\'' +
+                ", salt='" + salt + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
