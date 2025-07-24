92f961429a6cf5f53a36a1fd4b595141d6ff9247
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-07-23 23:14:36 -0300

    update user table creation script to include role constraint

--- src/main/resources/db/migration/V1__create-ser-table.sql
+++ src/main/resources/db/migration/V1__create-ser-table.sql
@@ -1,8 +1,8 @@
---Criação da tabela "user" (user entre aspas duplas, pois é uma palavra reservada do PostgreSQL)
+--Criação da tabela "user" (user entre aspas duplas, pois é uma palavra reservada do PostgresSQL)
 CREATE TABLE IF NOT EXISTS "user" (
     user_id BIGSERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     hash VARCHAR(255) NOT NULL,
     salt VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL
+    role VARCHAR(50) NOT NULL CHECK (role IN ('USER', 'ADMIN'))
 );
