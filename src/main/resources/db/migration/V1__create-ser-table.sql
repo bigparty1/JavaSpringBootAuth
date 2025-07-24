608bfb090c0c4d4ade65ca82a4b34776600da511
Kauê Gomes <kaue.gomes@ufrgs.br>
2025-07-24 00:24:19 -0300

    add constraint comment to user role in table creation script

--- src/main/resources/db/migration/V1__create-ser-table.sql
+++ src/main/resources/db/migration/V1__create-ser-table.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS "user" (
     username VARCHAR(100) NOT NULL UNIQUE,
     hash VARCHAR(255) NOT NULL,
     salt VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL CHECK (role IN ('USER', 'ADMIN'))
+    role VARCHAR(50) NOT NULL CHECK (role IN ('USER', 'ADMIN')) -- Restrição para garantir que o papel seja 'USER' ou 'ADMIN'
 );
